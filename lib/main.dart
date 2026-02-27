d1c641ad6cd62f6737899378343b12704879e5d9
gordoflowrd <gordoflowrd@gmail.com>
2026-02-27 18:39:02 +0000

    Update Flutter admin project

--- lib/main.dart
+++ lib/main.dart
@@ -1,17 +1,17 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(const SuperBettApp());
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+class SuperBettApp extends StatelessWidget {
+  const SuperBettApp({super.key});
 
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'SuperBett - Panel Administrativo',
       theme: ThemeData(
         // This is the theme of your application.
         //
